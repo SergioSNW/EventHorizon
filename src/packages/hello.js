3d5d65bc1cf95ec714aaeb7c90a0fe00fb73cf80
Sergio Santamaria Nora <synssn@gmail.com>
2022-10-11 20:56:25 +0100

    Update

---
+++ src/packages/hello.js
@@ -0,0 +1,33 @@
+import { LitElement, html, css } from 'lit-element';
+
+export class MeApp extends LitElement {
+
+    static get styles() {
+        return [
+            css`
+        /* your styles here
+         h1 { display: block;}
+        */
+      `,
+
+        ];
+    }
+
+    static get properties() {
+        return {
+            // your props here
+        };
+    }
+
+    constructor() {
+        super();
+
+        // property defaults etc here
+    }
+
+    render() {
+        return html`
+            <h1>Hello LitElement App!</h1>
+        `;
+    }
+}
