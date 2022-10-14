dba3a8bb07cba7f4f1c356833d9f30838ea435de
Juan <jcsantamar@gmail.com>
2022-10-14 18:42:23 +0200

    Competado el entorno WebPack para CSS, SCSS y SASS. Funcionando

--- src/eventhorizon.js
+++ src/eventhorizon.js
@@ -1,1 +1,3 @@
 alert('Howdy');
+import './pr_Juan/oo1.css';
+import './pr_Juan/zz1.sass';
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,36 @@
+//** Version para que WebPack trabaje con CSS, SCSS y SASS */
+
 const path = require('path');
-//const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-//const MiniCssExtractPlugin = require("css-minimizer-webpack-plugin");
-const OptimizeCSSAssetsPlugin = require("css-minimizer-webpack-plugin");
-// const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
-    mode: "production",
-    entry: ['./style.css', './src/eventhorizon.js'],     //ppp
-    // entry: ['./style.css', './src/eventhorizon.js','./src/pr_Juan/zz1.sass'],   //-- metiendo el sass da error
+    entry: ['./style.css', './src/eventhorizon.js'], 
+
+    mode: "production",    
+    mode: 'development',    // Para produccion comentar esta linea (en DEV, permite ver algo mas de 1 linea en el fich.output)
+
     output: {
         filename: './dist/js/main.min.js',
         path: path.resolve(__dirname)
     },
     module: {
         rules: [
-            // {
-            //     test: /\.js$/, exclude: /node_modules/,
-            //     use: {
-            //         loader: "babel-loader", 
-            //         options: { presets: ['n'] } 
-            //     }
-            // },
             {
-                test: /\.sass$/, use: 'sass-loader'
-            }, 
+                test: /\.html$/, use: 'html-loader'   
+            },
             {
-                test: /\.css$/, use: 'css-loader'
-            // }, 
-            // {
-            //     test: /\.(sass|scss)$/,
-            //     use: [new OptimizeCSSAssetsPlugin, 'sass-loader', 'scss-loader']
-            } 
+                test: /\.(c|sc|sa)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
         ]
-    }
-};
+    },
+    plugins: [
+        new MiniCssExtractPlugin()
+    ]
+}
