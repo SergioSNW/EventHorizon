b2f1d92bdf57d09bc05f8021ce95a8e5a991c549
Sergio Santamaria Nora <synssn@gmail.com>
2022-10-17 19:56:59 +0100

    Update Child Theme

--- functions.php
+++ functions.php
@@ -1,70 +1,19 @@
 <?php
-/**
- * Twenty Twenty-Two functions and definitions
- *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
- * @package WordPress
- * @subpackage Twenty_Twenty_Two
- * @since Twenty Twenty-Two 1.0
- */
 
+// Enqueueing Parent styles and support
+function enqueue_styles_child_theme() {
 
-if ( ! function_exists( 'twentytwentytwo_support' ) ) :
+	$parent_style = 'parent-style';
+	$child_style  = 'child-style';
 
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * @since Twenty Twenty-Two 1.0
-	 *
-	 * @return void
-	 */
-	function twentytwentytwo_support() {
+	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
 
-		// Add support for block styles.
-		add_theme_support( 'wp-block-styles' );
-
-		// Enqueue editor styles.
-		add_editor_style( 'style.css' );
-
-	}
-
-endif;
-
-add_action( 'after_setup_theme', 'twentytwentytwo_support' );
-
-if ( ! function_exists( 'twentytwentytwo_styles' ) ) :
-
-	/**
-	 * Enqueue styles.
-	 *
-	 * @since Twenty Twenty-Two 1.0
-	 *
-	 * @return void
-	 */
-	function twentytwentytwo_styles() {
-		// Register theme stylesheet.
-		$theme_version = wp_get_theme()->get( 'Version' );
-
-		$version_string = is_string( $theme_version ) ? $theme_version : false;
-		wp_register_style(
-			'twentytwentytwo-style',
-			get_template_directory_uri() . '/style.css',
-			array(),
-			$version_string
-		);
-
-		// Enqueue theme stylesheet.
-		wp_enqueue_style( 'twentytwentytwo-style' );
-
-	}
-
-endif;
-
-add_action( 'wp_enqueue_scripts', 'twentytwentytwo_styles' );
-
-// Add block patterns
-require get_template_directory() . '/inc/block-patterns.php';
+	wp_enqueue_style( $child_style, get_stylesheet_directory_uri() . '/style.css',
+		array( $parent_style ),
+	wp_get_theme()->get('Version')
+	);
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_styles_child_theme' );
 
 
 
